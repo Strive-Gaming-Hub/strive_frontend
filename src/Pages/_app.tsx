5a39cb4a84adb61b637ed309f7cbaef97c68cc76
Ak4shKr <akashkmr7667@gmail.com>
2024-07-03 17:20:25 +0530

    Add snackbar , changed password also complete

--- src/Pages/_app.tsx
+++ src/Pages/_app.tsx
@@ -3,6 +3,8 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import Layout from "../app/components/Common/Layout";
 import { Rubik } from "next/font/google";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import "../app/globals.css";
 import Loader from "@/app/components/common/Loader";
@@ -25,10 +27,19 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <div className={`min-h-screen w-full font-rubik ${rubik.variable}`}>
-      {shouldRenderSidebar && <Layout></Layout>}
-      {loader && <Loader />}
-      <Component {...pageProps} setLoader={setLoader} />
-      {/* </div> */}
+      {shouldRenderSidebar && (
+        <Layout>
+          {loader && <Loader />}
+          <Component {...pageProps} setLoader={setLoader} />
+          <ToastContainer
+            theme="dark"
+            closeOnClick
+            hideProgressBar
+            style={{ fontSize: "14px" }}
+          />
+          {/* </div> */}
+        </Layout>
+      )}
     </div>
   );
 }
--- src/app/components/Common/SideBAr.tsx
+++ src/app/components/Common/SideBAr.tsx
@@ -2,8 +2,21 @@ import React, { useState } from "react";
 import Link from "next/link";
 
 const Sidebar = () => {
+  const [changePassword, setChangePassword] = useState(false);
+
+  const handleChangePassword = () => {
+    setChangePassword(true);
+  };
+
+  const handleCloseModal = () => {
+    setChangePassword(false);
+  };
+
   return (
-    <div className="min-h-screen bg-[#11112B] text-white w-full rounded-xl  md:w-64 flex flex-col" style={{height: "calc(100% - 4.5rem)"}}>
+    <div
+      className="min-h-screen bg-[#11112B] text-white w-full rounded-xl  md:w-64 flex flex-col"
+      style={{ height: "calc(100% - 4.5rem)" }}
+    >
       {/* Menu Options */}
       <ul className="py-2">
         <li className="px-2 py-2 flex flex-row cursor-pointer group text-[#8E84A3] text-[16px] hover:bg-[#1F1943] hover:text-[#FFFFFF] rounded-md ">
@@ -34,6 +47,11 @@ const Sidebar = () => {
         <li className="px-2 py-2 flex flex-row cursor-pointer group text-[#8E84A3] text-[16px] hover:bg-[#1F1943] hover:text-[#FFFFFF] rounded-md">
           <Link href="/">
             <span>Help & Support</span>
+          </Link>
+        </li>
+        <li className="px-2 py-2 flex flex-row cursor-pointer group text-[#8E84A3] text-[16px] hover:bg-[#1F1943] hover:text-[#FFFFFF] rounded-md">
+          <Link href="/changepassword">
+            <span>Change Password</span>
           </Link>
         </li>
       </ul>
