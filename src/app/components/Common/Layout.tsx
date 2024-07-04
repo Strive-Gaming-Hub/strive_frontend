161a4f701ac3559ad5268fa4fc6cee8e7cf16f84
Ak4shKr <akashkmr7667@gmail.com>
2024-07-04 17:55:46 +0530

    Add prettier changes in Layout.tsx

--- src/app/components/Common/Layout.tsx
+++ src/app/components/Common/Layout.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import NavBar from '../Common/NavBar';
-import SideBar from '../Common/SideBAr';
+import React from "react";
+import NavBar from "../Common/NavBar";
+import SideBar from "../Common/SideBAr";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="bg-[#090C23] flex flex-col" >
+    <div className="bg-[#090C23] flex flex-col">
       <div className="flex-none">
         <NavBar />
       </div>
@@ -16,13 +16,15 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <div className="flex gap-2 p-2 flex-1">
         <SideBar />
 
-        <div className="flex flex-col flex-1 gap-2 overflow-y-auto"
-         style={{ height: "calc(100vh - 5.5rem)" }}>
+        <div
+          className="flex flex-col flex-1 gap-2 overflow-y-auto"
+          style={{ height: "calc(100vh - 5.5rem)" }}
+        >
           {children}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Layout;
