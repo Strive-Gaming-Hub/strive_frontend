eca439305b5e4424a0ab2a37eb1c48723241335e
Ak4shKr <akashkmr7667@gmail.com>
2024-07-04 17:56:37 +0530

    Add toasts in changepassword and login

--- src/Pages/login.tsx
+++ src/Pages/login.tsx
@@ -4,7 +4,6 @@ import { IoClose } from "react-icons/io5";
 import { getGoogleLoginUrl, striveLogin } from "@/Auth/Login";
 import { showToast } from "@/app/notifier/toast";
 import Link from "next/link";
-
 const Login = ({ setLoader = (t: boolean) => {} }) => {
   const [error, setError] = React.useState<string>("");
   const [googleLoginMessage, setGoogleLoginMessage] =
@@ -45,13 +44,15 @@ const Login = ({ setLoader = (t: boolean) => {} }) => {
         localStorage.setItem("user", JSON.stringify(res.data.userData));
         localStorage.setItem("accessToken", res.data.access_token);
         localStorage.setItem("refreshToken", res.data.refresh_token);
+        showToast("Login successful", "success");
         window.location.href = "/";
       } else {
         setError(res.message);
-        showToast("invalid credential,  try again!!", "error");
+        showToast("invalid credential, try again!!", "error");
       }
     } catch (error) {
       console.error("Login error:", error);
+      showToast("An error occurred during login.", "info");
       setError("An error occurred during login.");
     } finally {
       setLoader(false); // Set loading state back to false
@@ -69,9 +70,11 @@ const Login = ({ setLoader = (t: boolean) => {} }) => {
         window.location.href = url;
       } else {
         console.log(response.status, response.message);
+        showToast("An error during Google login.", "error");
       }
     } catch (error) {
       console.error("Google login error:", error);
+      showToast("An error on server!", "info");
     } finally {
       setLoader(false);
     }
@@ -84,81 +87,80 @@ const Login = ({ setLoader = (t: boolean) => {} }) => {
   }, []);
 
   return (
-
-      <div className=" bg-[#11112B] rounded-2xl flex flex-col items-center justify-center p-4 m-auto">
-        <form
-          className="w-full relative rounded"
-          id="login"
-          onSubmit={handleLogin}
-        >
-          <h2 className="text-[1.25rem] mb-1 text-center font-medium text-[#FFFFFF] leading-[30px]">
-            Sign in
-          </h2>
-          <div className="absolute top-[0.5rem] right-[0.5rem]">
-            <IoClose className="text-[#8E84A3] font-bold text-lg hover:scale-125 hover:cursor-pointer" />
-          </div>
-          <div className="mb-4">
-            <label className="text-[#FFFFFF] text-sm font-medium mb-2">
-              Email/username
-            </label>
-            <input
-              type="text"
-              name="credential"
-              placeholder="Enter email"
-              required={true}
-              className="shadow appearance-none rounded-lg w-full h-[38px] py-2 px-3 mt-1 bg-[#090C23] text-[#9094A6] text-[0.88rem] leading-tight focus:outline-1 focus:shadow-outline"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="text-[#FFFFFF] text-sm font-medium mb-2">
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Enter password"
-              required={true}
-              className="shadow appearance-none rounded-lg w-full h-[38px] py-2 px-3 mt-1 bg-[#090C23] text-[#9094A6] text-[0.88rem] leading-tight focus:outline-1 focus:shadow-outline"
-            />
-          </div>
-          <div className="flex items-end justify-end">
-            <p className="mb-4 text-[#8E84A3] font-medium text-[0.9rem]">
-              <a href="">Forget your password?</a>
-            </p>
-          </div>
-          <p className="text-[#FF0000] text-sm">{error}</p>
-          <div className="flex items-center justify-between">
-            <button
-              type="submit"
-              className="bg-[#9562FF] border-[#A77CFF] text-white text-[1rem] font-medium w-full py-2 px-5 rounded-[0.625rem] focus:outline-none focus:shadow-outline"
-            >
-              {loading ? "Logging in..." : "Continue"}
-            </button>
-          </div>
-          <p className="text-[#3f3f6a] text-[0.8rem] font-medium py-2 w-fit m-auto">
-            OR
+    <div className=" bg-[#11112B] rounded-2xl flex flex-col items-center justify-center p-4 m-auto w-80">
+      <form
+        className="w-full relative rounded"
+        id="login"
+        onSubmit={handleLogin}
+      >
+        <h2 className="text-[1.25rem] mb-1 text-center font-medium text-[#FFFFFF] leading-[30px]">
+          Sign in
+        </h2>
+        <div className="absolute top-[0.5rem] right-[0.5rem]">
+          <IoClose className="text-[#8E84A3] font-bold text-lg hover:scale-125 hover:cursor-pointer" />
+        </div>
+        <div className="mb-4">
+          <label className="text-[#FFFFFF] text-sm font-medium mb-2">
+            Email/username
+          </label>
+          <input
+            type="text"
+            name="credential"
+            placeholder="Enter email"
+            required={true}
+            className="shadow appearance-none rounded-lg w-full h-[38px] py-2 px-3 mt-1 bg-[#090C23] text-[#9094A6] text-[0.88rem] leading-tight focus:outline-1 focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="text-[#FFFFFF] text-sm font-medium mb-2">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            required={true}
+            className="shadow appearance-none rounded-lg w-full h-[38px] py-2 px-3 mt-1 bg-[#090C23] text-[#9094A6] text-[0.88rem] leading-tight focus:outline-1 focus:shadow-outline"
+          />
+        </div>
+        <div className="flex items-end justify-end">
+          <p className="mb-4 text-[#8E84A3] font-medium text-[0.9rem]">
+            <a href="">Forget your password?</a>
           </p>
-        </form>
-        <div className="w-full flex items-center justify-between">
+        </div>
+        <p className="text-[#FF0000] text-sm">{error}</p>
+        <div className="flex items-center justify-between">
           <button
-            onClick={handleGoogleLogin}
-            className="border-2 border-solid border-[#3e3e6a] text-white font-medium text-[1rem] w-full py-2 px-4 rounded-[0.625rem]"
+            type="submit"
+            className="bg-[#9562FF] border-[#A77CFF] text-white text-[1rem] font-medium w-full py-2 px-5 rounded-[0.625rem] focus:outline-none focus:shadow-outline"
           >
-            {loading ? "Please wait..." : "Continue with Google"}
+            {loading ? "Logging in..." : "Continue"}
           </button>
         </div>
-        <p className="text-[#8E84A3] mt-4 w-fit m-auto text-[0.8rem] font-medium">
-          New user?{" "}
-          <span className="text-white border-b-2">
-          <Link href="/register">Create an account</Link>
-          </span>
+        <p className="text-[#3f3f6a] text-[0.8rem] font-medium py-2 w-fit m-auto">
+          OR
         </p>
-        {googleLoginMessage && (
-          <p className="text-[#FFFFFF] mt-4 w-fit m-auto text-[0.8rem] font-medium">
-            {googleLoginMessage}
-          </p>
-        )}
+      </form>
+      <div className="w-full flex items-center justify-between">
+        <button
+          onClick={handleGoogleLogin}
+          className="border-2 border-solid border-[#3e3e6a] text-white font-medium text-[1rem] w-full py-2 px-4 rounded-[0.625rem]"
+        >
+          {loading ? "Please wait..." : "Continue with Google"}
+        </button>
       </div>
+      <p className="text-[#8E84A3] mt-4 w-fit m-auto text-[0.8rem] font-medium">
+        New user?{" "}
+        <span className="text-white border-b-2">
+          <Link href="/register">Create an account</Link>
+        </span>
+      </p>
+      {googleLoginMessage && (
+        <p className="text-[#FFFFFF] mt-4 w-fit m-auto text-[0.8rem] font-medium">
+          {googleLoginMessage}
+        </p>
+      )}
+    </div>
   );
 };
 
