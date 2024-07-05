8271d3ddc16e9b0680899140fdde783f7d5d46be
iGaurav17 <igauravbhardwaj17@gmail.com>
2024-07-05 18:37:38 +0530

    Fix state management of Google login

--- src/Auth/GoogleAuth.ts
+++ src/Auth/GoogleAuth.ts
@@ -1,5 +1,7 @@
 import { api } from "./client";
 import Cookies from "js-cookie";
+import { token, userData } from "./interfaces";
+import { createUserSession } from "./UserSession";
 
 export const handleOAuthCallback = async (code: string) => {
   if (code) {
@@ -7,15 +9,24 @@ export const handleOAuthCallback = async (code: string) => {
       //   Sends the code URL query to the backend server
       const response = await api.put("/api/v1/auth/exchange?code=" + code);
 
-      console.log(response.data);
+      // console.log(response.data);
 
       // Extract tokens from the response
-      const { access_token, refresh_token } = response.data.data;
-      // console.log(access_token, refresh_token);
-      // Store the extracted tokens in cookies
-      Cookies.set("accessToken", access_token);
-      Cookies.set("refreshToken", refresh_token);
-
+      if (response.status === 200) {
+        const userData: userData = {
+          email: response.data.data.email,
+          username: response.data.data.username,
+          phone: response.data.data.phone,
+          dob: response.data.data.dob,
+        }
+        const token: token = {
+          access_token: response.data.data.access_token,
+          refresh_token: response.data.data.refresh_token,
+        };
+        console.log(userData, token);
+        createUserSession(userData, token);
+        return response.data;
+      }
       // Redirect to the user data form page
       return true;
     } catch (error) {
@@ -26,6 +37,9 @@ export const handleOAuthCallback = async (code: string) => {
   }
 };
 
+
+
+
 export const getGoogleAuthUrl = async () => {
   try {
     const response = await api.get("/api/v1/auth/GoogleAuthUrl");
@@ -33,6 +47,6 @@ export const getGoogleAuthUrl = async () => {
     return response.data;
   } catch (error: any) {
     console.error("Error fetching Google OAuth URL:", error);
-    return error.response.data;
+    return error.response?.data;
   }
 };
