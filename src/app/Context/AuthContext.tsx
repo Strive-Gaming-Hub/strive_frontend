b9c97f5e08f6d6be75ec02d144510342a2084bd2
iGaurav17 <igauravbhardwaj17@gmail.com>
2024-07-06 17:35:33 +0530

    Fix Session Management

--- src/app/Context/AuthContext.tsx
+++ src/app/Context/AuthContext.tsx
@@ -1,22 +1,36 @@
 // app/context/AuthContext.tsx
 import React, { createContext, useState, useEffect, ReactNode } from "react";
+import Cookies from "js-cookie";
 
 interface AuthContextProps {
   isAuthenticated: boolean;
   login: () => void;
-  logout: () => void;
+  refreshToken: string | null;
+  userData: any;
 }
 
 const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [refreshToken, setRefreshToken] = useState<string | null>(null);
+  const [userData, setUserData] = useState<any>();
 
   useEffect(() => {
     // Check if user is already logged in
-    const user = localStorage.getItem("user");
-    if (user) {
+    const userData = localStorage.getItem("userData");
+    // parse json data
+    const parsedUserData = userData? JSON.parse(userData) : null;
+
+    // Check if refresh token is stored in cookies
+    const storedRefreshToken = Cookies.get("refreshToken");
+    console.log("User data from localStorage:", userData);
+    console.log("Refresh token from Cookies:", storedRefreshToken);
+
+    if (userData && storedRefreshToken && parsedUserData.username) {
       setIsAuthenticated(true);
+      setUserData(parsedUserData);
+      setRefreshToken(storedRefreshToken);
     }
   }, []);
 
@@ -24,15 +38,8 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     setIsAuthenticated(true);
   };
 
-  const logout = () => {
-    localStorage.removeItem("user");
-    localStorage.removeItem("accessToken");
-    localStorage.removeItem("refreshToken");
-    setIsAuthenticated(false);
-  };
-
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, login, refreshToken, userData }}>
       {children}
     </AuthContext.Provider>
   );
